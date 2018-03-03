f4053673af83dd15fa72e16d1b88a317a7ad514c
TrillCyborg <jasondwerner@gmail.com>
2018-03-03 21:12:40 +0200

    change base currency

--- index.js
+++ index.js
@@ -2,24 +2,47 @@ import _ from 'lodash'
 import axios from 'axios'
 import colors from 'colors';
 
-const BASE_AMOUNT = process.argv[2] || 1
-const BASE = 'ETH'
 const QUOTES = ['BTC', 'LTC', 'BCH', 'BTG']
+
+const getBaseAndAmount = () => {
+  const args = process.argv.slice(2, 4);
+  if (args.length === 2) {
+    return {
+      base: args[0],
+      amount: args[1],
+    }
+  } else if (args.length === 1 && !isNaN(Number(args[0]))) {
+    return {
+      base: 'ETH',
+      amount: args[0],
+    }
+  } else if (args.length === 1) {
+    return {
+      base: args[0],
+      amount: 1,
+    }
+  } else {
+    return {
+      base: 'ETH',
+      amount: 1,
+    }
+  }
+}
 
 const bit2CTicker = (quote) =>
   axios.get(`https://bit2c.co.il/Exchanges/${quote}Nis/Ticker.json`)
 
-const shapeShiftMarket = (quote) =>
-  axios.get(`https://shapeshift.io/marketinfo/${_.toLower(BASE)}_${quote}`)
+const shapeShiftMarket = (base, quote) =>
+  axios.get(`https://shapeshift.io/marketinfo/${_.toLower(base)}_${quote}`)
 
-const findRate = async (coin) => {
-  const { data: ssMarket } = await shapeShiftMarket(_.toLower(coin))
+const findRate = async ({ coin, base, amount }) => {
+  const { data: ssMarket } = await shapeShiftMarket(_.toLower(base), _.toLower(coin))
   const { data: b2cTicker } = await bit2CTicker(_.upperCase(coin))
-  const coinToNis = ((BASE_AMOUNT * ssMarket.rate) - (ssMarket.minerFee * 2)) * b2cTicker.l
-  const coinToNisAvg = ((BASE_AMOUNT * ssMarket.rate) - (ssMarket.minerFee * 2)) * b2cTicker.av
+  const coinToNis = ((amount * ssMarket.rate) - (ssMarket.minerFee * 2)) * b2cTicker.l
+  const coinToNisAvg = ((amount * ssMarket.rate) - (ssMarket.minerFee * 2)) * b2cTicker.av
   return {
-    amount: BASE_AMOUNT,
-    base: BASE.toUpperCase(),
+    amount: amount,
+    base: base.toUpperCase(),
     quote: _.toUpper(coin),
     shapeShiftRate: ssMarket.rate,
     bit2CRate: b2cTicker.l,
@@ -31,9 +54,10 @@ const findRate = async (coin) => {
 }
 
 (async () => {
-  const coinData = await Promise.all(QUOTES.map(findRate))
+  const { base, amount } = getBaseAndAmount();
+  const coinData = await Promise.all(QUOTES.map((coin) => findRate({ coin, base, amount })))
   const bestCoin = _.maxBy(coinData, 'coinToNis')
-  console.log(`💸  Best coin is ${bestCoin.quote.cyan}. ${BASE_AMOUNT} ${BASE} = ${bestCoin.coinToNis.cyan} NIS 💸`)
+  console.log(`💸  Best coin is ${bestCoin.quote.cyan}. ${amount} ${base} = ${bestCoin.coinToNis.cyan} NIS 💸`)
   console.log('------------')
   _.orderBy(coinData, 'coinToNis', 'desc').forEach(({ amount, base, quote, coinToNis, coinToNisAvg, minerFee }, i) =>
     console.log(`${i + 1}. ${quote}: ${coinToNis} NIS ------ 24h Avg: ${coinToNisAvg} ------ Miner Fee: ${minerFee}`))
