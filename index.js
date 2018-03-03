a51beaa3bca749a3570e0c3e8a3f11514f9be6b9
TrillCyborg <jasondwerner@gmail.com>
2018-03-03 18:54:12 +0200

    script works

---
+++ index.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+import axios from 'axios';
+
+const BASE_AMOUNT = process.argv[2] || 1;
+const BASE = 'ETH';
+const QUOTES = ['BTC', 'LTC', 'BCH'];
+
+const bit2CTicker = (quote) =>
+  axios.get(`https://bit2c.co.il/Exchanges/${quote}Nis/Ticker.json`)
+
+const shapeShiftMarket = (quote) =>
+  axios.get(`https://shapeshift.io/marketinfo/${_.toLower(BASE)}_${quote}`)
+
+Promise.all(QUOTES.map(async (coin) => {
+  const { data: ssMarket } = await shapeShiftMarket(_.toLower(coin));
+  const { data: b2cTicker } = await bit2CTicker(_.upperCase(coin));
+  const coinToNis = ((BASE_AMOUNT * ssMarket.rate) - (ssMarket.minerFee * 2)) * b2cTicker.l
+  return {
+    amount: BASE_AMOUNT,
+    base: BASE.toUpperCase(),
+    quote: _.toUpper(coin),
+    shapeShiftRate: ssMarket.rate,
+    bit2CRate: b2cTicker.l,
+    bit2CLast: b2cTicker.ll,
+    minerFee: ssMarket.minerFee,
+    coinToNis: coinToNis.toFixed(2),
+  }
+})).then(data => {
+  const bestCoin = _.maxBy(data, 'coinToNis');
+  console.log(`Best coin is ${bestCoin.quote}. ${BASE_AMOUNT} ${BASE} = ${bestCoin.coinToNis} NIS`)
+  console.log('------------')
+  data.forEach(({ amount, base, quote, coinToNis }) =>
+    console.log(`${quote}: ${coinToNis} NIS`))
+});
